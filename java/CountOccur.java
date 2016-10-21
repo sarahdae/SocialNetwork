04a1c465db62431137f7678b5beea4b9787ba0db
AnRueKe <anke.rueb@t-online.de>
2016-10-21 11:16:39 +0200

    Update CountOccur.java

--- java/CountOccur.java
+++ java/CountOccur.java
@@ -1,8 +1,6 @@
 
 /**
- * zählt, wie oft die Personen in jedem Kapitel erwähnt wird wenn in der vis das
- * jeweilige kapitel ausgewählt wird, wird das programm von neuem starten
- * output:JSON file with data
+ * zählt, wie oft die Person im Buch in jedem Kapitel erwähnt wird
  */
 import java.io.BufferedReader;
 import java.io.File;
@@ -12,98 +10,57 @@ import java.util.*;
 
 public class CountOccur {
 
-    HashMap<String, Integer> entities = new HashMap<String, Integer>();
-    HashMap<String, Integer> events = new HashMap<String, Integer>();
-    ArrayList chaps = new ArrayList();
+    private HashMap<String, Integer> counter = new HashMap<String, Integer>();
 
-    public CountOccur(CheckChars nnpList) {
-        entities = this.counting(nnpList);
-    }
+    public CountOccur(CheckChars entities, int chapter) {
 
-    private HashMap<String, Integer> counting(CheckChars nnpList) {
+        if(chapter != 0) {
+            int chap = chapter;
+        }
+
         try {
             String lemma = "";
-            String chapterID = "";
-            String sentenceID = "";
-            String chapNum = ""; //only digits as String
-            String newKey ="";
-            ArrayList nnps = nnpList.getCheckedCharacters();
-            List<String> chapID = new ArrayList();
-            List<String> lemmaList = new ArrayList();
-            
-            for (int i = 0; i < nnps.size(); i++) { //LOOP TO GO THROUGH LIST OF NNPS
-                String[] nnpParts = (String[]) nnps.get(i);//convert object into String[]
-                chapterID = nnpParts[0]; //chapterID
-                sentenceID = nnpParts[1];
-                lemma = nnpParts[2]; //only lemma from filtered nnps
-                //System.out.println(lemma); //lemmas are all there
-                lemmaList = Arrays.asList(lemma); // convert lemmaString into lemmaList
-                chapID = Arrays.asList(chapterID); //
-                
-                String currChap= "1"; //starting from one
-                String currSent="1";
-                
-                //I NEED INPUT NUMBER (as String) OF VIS SLIDER (chapNum)
-                //only counting one chapter by one
-                if (chapterID.equals(currChap)) {
-                    for (String name : lemmaList) { //go through lemmaList
-                        Integer oldCount = entities.get(name);
-                        if (oldCount == null) {
-                            oldCount = 0;
-                        }
-                        //add lemma and number of occurences into hashmap
-                        entities.put(lemma, oldCount + 1);
-                    }
-                    
-                }
-                    //EVENT COUNTING STARTS HERE
-                    //if two or more entities exist in same or the next sentence
-                    //new event is created
-                //e.g.: given chapter1: in sentence 
-                if (sentenceID.equals(currSent)){ //choose sentence
-                    for(String event : chapID){
-                        Integer evCount = events.get(event);
-                           if (evCount == null) {
-                            evCount = 0;
-                        }
-                        events.put(chapterID, evCount + 1);
-                        chaps.add(chapterID);
+            ArrayList realCharList = entities.getCheckedCharacters();
+            //System.out.println("chapID\tlemma");
+            for (int i = 0; i < realCharList.size(); i++)//go through list of filtered nnps
+            {
+                String[] nnpParts = (String[]) realCharList.get(i);//lemma from filtered nnps
+                String chapterID = nnpParts[0]; //chapterID
+                lemma = nnpParts[2].toLowerCase();
+                //System.out.println(chapterID + " " + lemma);
+                //while reading each line --> +1 whenever same name occurs
+                //http://stackoverflow.com/questions/4820716/finding-repeated-words-on-a-string-and-counting-the-repetitions
+
+                if((chapter == 0) || (chapter == Integer.valueOf(chapterID))) {
+                    if(counter.containsKey(lemma)) {
+                        int newVal = counter.get(lemma) + 1;
+                        counter.put(lemma, newVal);
+                    } else {
+                        counter.put(lemma, 0);
                     }
                 }
             }
 
-            //print hashmap line by line, for each chapter
-            System.out.println("========== chapter "+chapterID+"==========");
-            for (Map.Entry<String, Integer> entry : entities.entrySet()) {
-                if (entry.getValue() != 0) {
-                    System.out.println(entry.getKey() + " : " + entry.getValue());
-                }
-            }
-                        System.out.println("========== sentences "+chaps.toString()+"==========");
-                        for (Map.Entry<String, Integer> sth : events.entrySet()) {
-                if (sth.getValue() != 0) {
-                    System.out.println(sth.getKey() + " : " + sth.getValue());
-                }
-            }
-            //if lemma equals
+            // go through chapters
+            //if lemma equals 
             //reset value to 0 when new chapter begins
         } catch (Exception e) {
             System.out.println("wrong file");
         }
-        return entities;
     }
 
-    public HashMap getOccur() {
-        return entities;
+    public HashMap<String, Integer> getOccur() {
+        return counter;
     }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Filter filteredChars = new Filter("LotF_Annotated.tsv");
-        CheckChars check = new CheckChars(filteredChars, "LotFChars.txt");
-        CountOccur test = new CountOccur(check);
-    }
+    /*public static void main(String[] args) {
+        Filter filteredChars = new Filter("/home/anke/SocialNetwork/TextFiles/LotF_Annotated.tsv");
+        CheckChars entities = new CheckChars(filteredChars, "/home/anke/SocialNetwork/TextFiles/LotFChars.txt");
+        CountOccur test = new CountOccur(entities);
+
+    }*/
 
 }
