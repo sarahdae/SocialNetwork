384bbc5330100c6199be44b3cf232eadfe6044a3
Gnoeccsi <ankhuong.bui@hotmail.de>
2016-12-01 11:05:05 +0100

    Add files via upload

---
+++ java/Main.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Filter filteredCharacters = new Filter("E:\\E_Dokumente\\semester7\\wolskaProject\\WolskaProject\\src\\LotF_Annotated.tsv");
+        CheckChars checkChars = new CheckChars(filteredCharacters, "E:\\E_Dokumente\\semester7\\wolskaProject\\WolskaProject\\src\\LotFChars.txt");
+        CountOccur countOccur = new CountOccur(checkChars, 2);
+        countOccur.createJson();
+
+        //hashmap in der jeder character einer anzahl zugemappt ist, wie oft er mit der (im Hintergrund ermittelten)
+        //"Hauptperson" im selben Satz vorkommt.
+        //Die Hauptperson ist die am häufigsten genannte.
+        HashMap<String, Integer> occurrenceEdges = countOccur.getCooccurrenceEdges();
+        System.out.println(occurrenceEdges);
+    }
+
+
+}
