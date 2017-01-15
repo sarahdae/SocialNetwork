441ba3fe3c30fcae1fc3770c512777d33b3049e8
Gnoeccsi <ankhuong.bui@hotmail.de>
2017-01-16 00:53:42 +0100

    Update CountOccur.java

--- java/CountOccur.java
+++ java/CountOccur.java
@@ -4,10 +4,6 @@
  * jeweilige kapitel ausgewählt wird, wird das programm von neuem starten
  * output:JSON file with data made by Anke Rüb
  */
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
 import org.json.simple.JSONArray;
@@ -16,12 +12,12 @@ import org.json.simple.JSONObject;
 public class CountOccur {
 
     private HashMap<String, Integer> entities = new HashMap<>();
-    private HashMap<String, Integer> cooccurrenceEdges = new HashMap<>();
 
     private List<String> indexes; // for source and target tags in Json file
+    private List<String> targetIndexes;
     private ArrayList<String[]> nnps = new ArrayList<>();
     private Integer currentChapter;
-
+    
     public CountOccur(CheckChars nnpList, int currChap) {
         nnps = nnpList.getCheckedCharacters();
         currentChapter = currChap;
@@ -36,8 +32,7 @@ public class CountOccur {
             List<String> chapID;
             List<String> lemmaList = new ArrayList<>();
 
-            for (int i = 0; i < nnps.size(); i++) { //LOOP TO GO THROUGH LIST OF NNPS
-                String[] nnpParts = nnps.get(i);
+            for (String[] nnpParts : nnps) {
                 chapterID = nnpParts[0]; //chapterID
                 sentenceID = nnpParts[1];
                 lemma = nnpParts[2]; //only lemma from filtered nnps
@@ -70,20 +65,8 @@ public class CountOccur {
         }
         return entities;
     }
-
-    private String getMaxOccuring(HashMap<String, Integer> entities) {
-
-        String mainPerson = "";
-        Integer maxValueInMap = (Collections.max(entities.values()));
-        for (Map.Entry<String, Integer> entry : entities.entrySet()) {
-            if (entry.getValue() == maxValueInMap) {
-                mainPerson = (entry.getKey());
-            }
-        }
-        return mainPerson;
-    }
-
-    private List<Integer> getSentenceIds(String person) {
+    
+        private List<Integer> getSentenceIds(String person) {
         List<Integer> sentenceIds = new ArrayList<>();
         for (String[] entity : nnps) {
             if (Integer.parseInt(entity[0]) == currentChapter) {
@@ -95,54 +78,46 @@ public class CountOccur {
         return sentenceIds;
     }
 
-    public HashMap<String, Integer> getCooccurrenceEdges() {
+    public HashMap<String, HashMap<String, Integer>> getAllEdges() {
+        HashMap<String, HashMap<String, Integer>> listOfOccurenceEdges = new HashMap<>();
 
-        String mainPerson = getMaxOccuring(entities);
-        //A Hashmap mapping from every person to a number indicating how often she cooccurs with the main person!
-        List<Integer> sentenceIds = getSentenceIds(mainPerson);
+        for (String[] ent : nnps) {
 
-        for (String[] entity : nnps) {
-            if (Integer.parseInt(entity[0]) == currentChapter) {
-                //we add an edge/weight to the edge from the main person for every person occuring in the same or in adjacent sentences
-                if (sentenceIds.contains(Integer.parseInt(entity[1])) || sentenceIds.contains(Integer.parseInt(entity[1]) + 1)
-                        || (sentenceIds.contains(Integer.parseInt(entity[1]) - 1))) {
-                    String person = entity[2];
+            HashMap<String, Integer> cooccurrenceEdges = new HashMap<>();
+            List<Integer> sentenceIds = getSentenceIds(ent[2]);
 
+            for (String[] entity : nnps) {
+
+                if (Integer.parseInt(entity[0]) == currentChapter) {
+                    //we add an edge/weight to the edge from the main person for every person occuring in the same or in adjacent sentences
                     //as the main person. We create no edge from the main person to itself.
-                    if (person.contentEquals(mainPerson)) {
-                        continue;
-                    }
+                    if (sentenceIds.contains(Integer.parseInt(entity[1])) || sentenceIds.contains(Integer.parseInt(entity[1]) + 1)
+                            || (sentenceIds.contains(Integer.parseInt(entity[1]) - 1))) {
+                        String person = entity[2];
 
-                    if (cooccurrenceEdges.containsKey(person)) {
-                        int count = cooccurrenceEdges.get(person);
-                        count += 1;
-                        cooccurrenceEdges.put(entity[2], count);
-                    } else {
-                        cooccurrenceEdges.put(person, 1);
-                    }
+                        if (person.contentEquals(ent[2])) {
+                            continue;
+                        }
 
+                        if (cooccurrenceEdges.containsKey(person)) {
+                            int count = cooccurrenceEdges.get(person);
+                            count += 1;
+                            cooccurrenceEdges.put(entity[2], count);
+                        } else {
+                            cooccurrenceEdges.put(person, 1);
+                        }
+                    }
                 }
             }
+            listOfOccurenceEdges.put(ent[2], cooccurrenceEdges);
         }
-        System.out.println(mainPerson);
-        System.out.println(cooccurrenceEdges);
-
-        return cooccurrenceEdges;
+        return listOfOccurenceEdges;
     }
 
     public void createJson() {
         JSONObject root = new JSONObject(); //actual json file root
         JSONArray listNodes = new JSONArray(); //array for nodes
         JSONArray listLinks = new JSONArray(); //array for links
-
-        String sourcePerson = getMaxOccuring(entities);
-        int indexOfMain = 0;
-        Map<String, Integer> hash = new HashMap<>();
-        hash.put(sourcePerson, 0);
-        HashMap<String, Integer> getEdges = getCooccurrenceEdges();
-        for (Map.Entry<String, Integer> entry : hash.entrySet()) {
-            indexOfMain = indexes.indexOf(entry.getKey());
-        }
 
         //NODES
         for (Map.Entry<String, Integer> entry : entities.entrySet()) {
@@ -159,23 +134,26 @@ public class CountOccur {
         root.put("nodes", listNodes);
 
         //LINKS
-        for (Map.Entry<String, Integer> entry : cooccurrenceEdges.entrySet()) {
-            if (entry.getValue() != 0) {
-                JSONObject linksData = new JSONObject(); // data of source,target,value,length
-                linksData.put("source", indexOfMain);
-                linksData.put("target", (indexes.indexOf(entry.getKey())));
-                linksData.put("value", 1);
-                linksData.put("length", (entry.getValue() * 1000));
-                listLinks.add(linksData);
-
+        HashMap<String, HashMap<String, Integer>> getEdges = getAllEdges();
+        for (Map.Entry<String, HashMap<String, Integer>> entry : getEdges.entrySet()) {
+            indexes = new ArrayList<>(getEdges.keySet()); // <== Parse
+            HashMap<String, Integer> valueofGetEdges = entry.getValue();
+            for (Map.Entry<String, Integer> sth : valueofGetEdges.entrySet()) { //going through smaller hashmap aka value of listHashMap
+                targetIndexes = new ArrayList<>(valueofGetEdges.keySet()); // <== Parse
+                if (entry.getValue() != null) {
+                    JSONObject linksData = new JSONObject(); // data of source,target,value,length
+                    linksData.put("source", indexes.indexOf(entry.getKey()));
+                    linksData.put("target", targetIndexes.indexOf(sth.getKey()));
+                    linksData.put("value", 1);
+                    linksData.put("length", (sth.getValue() * 1000));
+                    listLinks.add(linksData);
+                }
             }
         }
         root.put("links", listLinks);
-
         //creates new json file and print solution into it
         try {
-            PrintWriter writer = new PrintWriter("web/countOccur.json",
-                    "UTF-8");
+            PrintWriter writer = new PrintWriter("web/countOcc.json", "UTF-8");
             writer.print(root);
             writer.close();
         } catch (Exception e) {
@@ -186,7 +164,5 @@ public class CountOccur {
     public HashMap getOccur() {
         return entities;
     }
-
-    public static void main(String[] args) {
-    }
 }
+
